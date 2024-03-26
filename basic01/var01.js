bfccb039c2c86f7855115bd5ab86d6b49fe4f152
pranavdwivedii <155006782+pranavdwivedii@users.noreply.github.com>
2024-03-26 13:40:38 +0000

    datatypes

---
+++ basic01/var01.js
@@ -0,0 +1,8 @@
+const accountId = 1212
+let accountEmail = "pran99@gmail.com"
+var accountPass = "12345" /*var is never used because of block and functional scope*/
+//accountId = 2 not allowed
+
+let accountState;
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass, accountState ])
